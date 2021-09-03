77aae4ec28df556abcdac52875f26189d6981cc7
Abel Gonçalves <abeldev56@gmail.com>
2021-09-03 01:46:22 -0300

    Inclusão de mais um exercicío

--- 100_exercicios_de_logica/55_desafio.js
+++ 100_exercicios_de_logica/55_desafio.js
@@ -7,5 +7,26 @@
         tentativas para tentar acertar.
 */
 
+var numeroSorteado = parseInt(Math.random() * 11);
+var numeroJogador = parseInt(window.prompt("Digite um número"));
+var contador = 1
+
+if(numeroSorteado === numeroJogador){
+    console.log("Acertou!")
+}else{
+    while(contador <=3){
+        numeroJogador = parseInt(window.prompt("Digite um número"));
+        if(numeroSorteado === numeroJogador){
+            console.log("Acertou!")
+        }else if(contador === 3){
+            console.log("Suas tentativas acabaram!!")
+        }
+        contador += 1
+    }
+    
+}
 
 
+
+
+console.log(numeroSorteado)
