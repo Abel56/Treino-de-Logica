3dd6bb80b9a3a5dcf5b9626c1a3bec621daaed50
Abel Gonçalves <abeldev56@gmail.com>
2022-02-14 14:17:07 -0300

    java
    Java

---
+++ estruturas_de_dados/arrays/array.js
@@ -0,0 +1,44 @@
+/* var daysOfWeek = new Array('Sunday','Monday','Tueday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'); */
+/* var daysOfWeek = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];    
+
+console.log(daysOfWeek[3]);
+
+for(var i = 0; i < daysOfWeek.length; i++){
+    console.log(daysOfWeek[i]);
+}
+
+var fibonacchi = [];
+fibonacchi[0] = 1
+fibonacchi[1] = 1
+fibonacchi[2] = 2
+
+
+for(var i = 3; i < 20; i++){
+    fibonacchi[i] = fibonacchi[i - 1] + fibonacchi[i - 2]
+    console.log(fibonacchi);
+}
+
+for (var i = 0; i < fibonacchi.length; i++){
+    console.log(fibonacchi[i]);
+}
+ */
+/* var numbers = [0,1,2,3,4,5,6,7,8,9];
+
+
+for (var i = 0; i < numbers.length; i++){
+    var soma = 0;
+
+    soma += numbers[i];
+    
+
+}
+console.log(soma); */
+/* console.log(numbers);
+
+numbers.push(15);
+numbers.unshift(-1);
+numbers.unshift(-2);
+
+console.log(numbers);
+ */
+
